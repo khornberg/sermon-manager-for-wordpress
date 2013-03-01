8bc1e4f64f99e2f3dc2c250b8888a7dd64706c7b
Jack Lamb <jack@wpforchurch.com>
2013-03-01 10:42:30 -0700

    shorten tax description to clean up page

--- includes/types-taxonomies.php
+++ includes/types-taxonomies.php
@@ -404,4 +404,129 @@ function wpfc_get_term_dropdown($taxonomy) {
 		}
 	}
 } 
+
+/*
+Taxonomy Short Description
+http://wordpress.mfields.org/plugins/taxonomy-short-description/
+Shortens the description shown in the administration panels for all categories, tags and custom taxonomies.
+V: 1.3.1
+Copyright 2011  Michael Fields  michael@mfields.org
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as published by
+the Free Software Foundation.
+
+Function names have been modified to prevent conflicts.
+*/
+
+// Actions.
+function wpfc_taxonomy_short_description_actions() {
+	$taxonomies = get_taxonomies();
+	foreach ( $taxonomies as $taxonomy ) {
+		$config = get_taxonomy( $taxonomy );
+		if ( isset( $config->show_ui ) && true == $config->show_ui ) {
+			add_action( 'manage_' . $taxonomy . '_custom_column', 'wpfc_taxonomy_short_description_rows', 10, 3 );
+			add_action( 'manage_edit-' . $taxonomy . '_columns',  'wpfc_taxonomy_short_description_columns' );
+			add_filter( 'manage_edit-' . $taxonomy . '_sortable_columns', 'wpfc_taxonomy_short_description_columns' );
+		}
+	}
+}
+add_action( 'admin_init', 'wpfc_taxonomy_short_description_actions' );
+
+
+// Term Columns.
+// Remove the default "Description" column. Add a custom "Short Description" column.
+function wpfc_taxonomy_short_description_columns( $columns ) {
+	$position = 0;
+	$iterator = 1;
+	foreach( $columns as $column => $display_name ) {
+		if ( 'name' == $column ) {
+			$position = $iterator;
+		}
+		$iterator++;
+	}
+	if ( 0 < $position ) {
+		/* Store all columns up to and including "Name". */
+		$before = $columns;
+		array_splice( $before, $position );
+
+		/* All of the other columns are stored in $after. */
+		$after  = $columns;
+		$after = array_diff ( $columns, $before );
+
+		/* Prepend a custom column for the short description. */
+		$after = array_reverse( $after, true );
+		$after['mfields_short_description'] = $after['description'];
+		$after = array_reverse( $after, true );
+
+		/* Remove the original description column. */
+		unset( $after['description'] );
+
+		/* Join all columns back together. */
+		$columns = $before + $after;
+	}
+	return $columns;
+}
+
+
+// Term Rows. - Display the shortened description in each row's custom column.
+function wpfc_taxonomy_short_description_rows( $string, $column_name, $term ) {
+	if ( 'mfields_short_description' == $column_name ) {
+		global $taxonomy;
+		$string = term_description( $term, $taxonomy );
+		$string = wpfc_taxonomy_short_description_shorten( $string, apply_filters( 'mfields_taxonomy_short_description_length', 130 ) );
+	}
+	return $string;
+}
+
+// Shorten a string to a given length.
+function wpfc_taxonomy_short_description_shorten( $string, $max_length = 23, $append = '&#8230;', $encoding = 'utf8' ) {
+
+	/* Sanitize $string. */
+	$string = strip_tags( $string );
+	$string = trim( $string );
+	$string = html_entity_decode( $string, ENT_QUOTES, 'UTF-8' );
+	$string = rtrim( $string, '-' );
+
+	/* Sanitize $max_length */
+	if ( 0 == abs( (int) $max_length ) ) {
+		$max_length = 23;
+	}
+
+	/* Return early if the php "mbstring" extension is not installed. */
+	if ( ! function_exists( 'mb_substr' ) ) {
+		$length = strlen( $string );
+		if ( $length > $max_length ) {
+			return substr_replace( $string, $append, $max_length );
+		}
+		return $string;
+	}
+
+	/* Count how many characters are in the string. */
+	$length = strlen( utf8_decode( $string ) );
+
+	/* String is longer than max-length. It needs to be shortened. */
+	if ( $length > $max_length ) {
+
+		/* Shorten the string to max-length */
+		$short = mb_substr( $string, 0, $max_length, $encoding );
+
+		/*
+		 * A word has been cut in half during shortening.
+		 * If the shortened string contains more than one word
+		 * the last word in the string will be removed.
+		 */
+		if ( 0 !== mb_strpos( $string, $short . ' ', 0, $encoding ) ) {
+			$pos = mb_strrpos( $short, ' ', $encoding );
+			if ( false !== $pos ) {
+				$short = mb_substr( $short, 0, $pos, $encoding );
+			}
+		}
+
+		/* Append shortened string with the value of $append preceeded by a non-breaking space. */
+		$string = $short . ' ' . $append;
+	}
+
+	return $string;
+}
 ?>
