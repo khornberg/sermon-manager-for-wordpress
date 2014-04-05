593028b9f18ac8a8473778ea5bf3773ddcab1038
Jack Lamb <jack@wpforchurch.com>
2014-04-04 22:51:40 -0700

    for taxonomy feeds display description as itunes:summary and image as the feed image.

--- includes/podcast-functions.php
+++ includes/podcast-functions.php
@@ -2,19 +2,11 @@
 
 // add the itunes namespace to the RSS opening element
 function wpfc_podcast_add_namespace() {
-	if( get_query_var( 'post_type' ) !== 'wpfc_sermon' )
-        return;
 	echo 'xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd/"';
 }
-add_action( 'rss_ns', 'wpfc_podcast_add_namespace' );
-add_action( 'rss2_ns', 'wpfc_podcast_add_namespace' );
-
 
 // add itunes specific info to each item
 function wpfc_podcast_add_head(){
-
-	if( get_query_var( 'post_type' ) !== 'wpfc_sermon' )
-        return;
 	
 	remove_filter('the_content', 'add_wpfc_sermon_content');
 
@@ -22,27 +14,33 @@ function wpfc_podcast_add_head(){
 	<copyright><?php echo esc_html( $settings['copyright'] ) ?></copyright>
 	<itunes:subtitle><?php echo esc_html( $settings['itunes_subtitle'] ) ?></itunes:subtitle>
 	<itunes:author><?php echo esc_html( $settings['itunes_author'] ) ?></itunes:author>
-	<itunes:summary><?php echo esc_html( $settings['itunes_summary'] ) ?></itunes:summary>
+	<?php $category_description = category_description();
+	if ( ! empty( $category_description ) ) { ?>
+		<itunes:summary><?php echo $category_description; ?></itunes:summary>
+	<?php } else{ ?>
+		<itunes:summary><?php echo esc_html( $settings['itunes_summary'] ) ?></itunes:summary>
+	<?php } ?>		
 	<itunes:owner>
 		<itunes:name><?php echo esc_html( $settings['itunes_owner_name'] ) ?></itunes:name>
 		<itunes:email><?php echo esc_html( $settings['itunes_owner_email'] ) ?></itunes:email>
 	</itunes:owner>
 	<itunes:explicit>no</itunes:explicit>
-	<itunes:image href="<?php echo esc_url( $settings['itunes_cover_image'] ) ?>" />
+	<?php //Show the taxonomy image if there is one
+	$image_url = apply_filters( 'sermon-images-queried-term-image-url', '' ); 
+	if ($image_url) { ?>
+		<itunes:image href="<?php echo $image_url ?>" />
+	<?php } else { //otherwise, show the image from the podcast settings ?>
+		<itunes:image href="<?php echo esc_url( $settings['itunes_cover_image'] ) ?>" />
+	<?php } ?>
 	<itunes:category text="<?php echo esc_attr( $settings['itunes_top_category'] ) ?>">
 		<itunes:category text="<?php echo esc_attr( $settings['itunes_sub_category'] ) ?>"/>
 	</itunes:category>
 	<?php
 }
-add_action('rss_head', 'wpfc_podcast_add_head');
-add_action('rss2_head', 'wpfc_podcast_add_head');
 
 // add itunes specific info to each item
 function wpfc_podcast_add_item(){
-
-	if( get_query_var( 'post_type' ) !== 'wpfc_sermon' )
-        return;
-		
+	
 	global $post;
 	
     $audio = get_post_meta($post->ID, 'sermon_audio', 'true');
@@ -84,9 +82,6 @@ function wpfc_podcast_add_item(){
 	<?php if( $topics ) { ?>
 		<itunes:keywords><?php echo esc_html( $topics ); ?></itunes:keywords><?php } 
 }
-add_action('rss_item', 'wpfc_podcast_add_item');
-add_action('rss2_item', 'wpfc_podcast_add_item');
-
 
 //Display the sermon description as the podcast summary
 function wpfc_podcast_summary ($content) {
@@ -95,15 +90,10 @@ function wpfc_podcast_summary ($content) {
 	$content = strip_tags( get_wpfc_sermon_meta('sermon_description') ); 
 		return $content;
 }
-add_filter( 'the_content_feed', 'wpfc_podcast_summary', 10, 3);
-add_filter( 'the_excerpt_rss', 'wpfc_podcast_summary');
 
 //Filter published date for podcast: use sermon date instead of post date
 function wpfc_podcast_item_date ($time, $d = 'U', $gmt = false) {
  
-	if( get_query_var( 'post_type' ) !== 'wpfc_sermon' && is_feed() )
-        return;
-	
 	$time = wpfc_sermon_date('D, d M Y H:i:s O');
 	return $time;
 }
@@ -113,6 +103,14 @@ function wpfc_modify_sermon_date( $query ) {
 	if ( !is_admin() && $query->is_main_query() && $query->is_feed() ) :
 	if( is_post_type_archive('wpfc_sermon') || is_tax( 'wpfc_preacher' ) || is_tax( 'wpfc_sermon_topics' ) || is_tax( 'wpfc_sermon_series' ) || is_tax( 'wpfc_bible_book' ) ) {
 		add_filter ( 'get_post_time', 'wpfc_podcast_item_date', 10, 3);
+		add_action( 'rss_ns', 'wpfc_podcast_add_namespace' );
+		add_action( 'rss2_ns', 'wpfc_podcast_add_namespace' );
+		add_action('rss_head', 'wpfc_podcast_add_head');
+		add_action('rss2_head', 'wpfc_podcast_add_head');
+		add_action('rss_item', 'wpfc_podcast_add_item');
+		add_action('rss2_item', 'wpfc_podcast_add_item');
+		add_filter( 'the_content_feed', 'wpfc_podcast_summary', 10, 3);
+		add_filter( 'the_excerpt_rss', 'wpfc_podcast_summary');
 	}
 	endif;
 }
